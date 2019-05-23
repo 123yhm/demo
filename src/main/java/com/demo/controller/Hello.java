505450091f7a9e3d5b7418e575d704d72066661b
阳浩明 <1343876419@qq.com>
2019-05-23 20:24:36 +0800

    引入thymeleaf模板引擎

--- src/main/java/com/demo/controller/Hello.java
+++ src/main/java/com/demo/controller/Hello.java
@@ -1,17 +1,25 @@
 package com.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @Auther: Administrator
  * @Date: 2019-05-22 13:50
  * @Description:
  */
-@RestController
+@Controller
 public class Hello {
+    /**
+     * 由thymeleaf模板进行解析的
+     * @return
+     */
     @RequestMapping("/hello")//测试
-    public String main(){
-        return "Hello word";
+    public String main(Map<String,Object> map){
+        map.put("hello","欢迎来到springboot世界，希望你学习节节攀升");
+        return "index";
     }
 }
