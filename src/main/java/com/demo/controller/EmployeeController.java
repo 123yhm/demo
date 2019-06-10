e74df0e05500d44992df82e036f0933f1c9de2d0
阳浩明 <1343876419@qq.com>
2019-06-10 23:33:51 +0800

    1、解决模板解析错误问题，去掉多余的JSJQ代码防止页面代码重复 2、完成员工同页面修改功能

--- src/main/java/com/demo/controller/EmployeeController.java
+++ src/main/java/com/demo/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -31,7 +33,7 @@ public class EmployeeController {
      * @param model
      * @return
      */
-    @GetMapping(value = "/user/emps")
+    @GetMapping(value = "/emps")
     public String list(Model model){
         System.out.println("进入员工操作后台");
         Collection<Employee> empList = employeedao.getAll();
@@ -46,15 +48,43 @@ public class EmployeeController {
      * @param model
      * @return
      */
-    @GetMapping(value ="/user/emp")
+    @GetMapping(value ="/emp")
     public  String getEmp(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("dept",departments);
+
+        return "emp/listadd";
+    }
+
+    /**
+     * 员工添加
+     * @param employee
+     * @return
+     */
+    @PostMapping(value = "/empAdds")
+    public String empAdd(Employee employee){
+        employeedao.save(employee);
+        return "redirect:/emps";
+    }
+    /**
+     * 员工修改跳转显示
+     */
+    @GetMapping(value = "/emp/{id}")
+    public String empEdit(@PathVariable("id") Integer id, Model model){
+        //先查询当前员工信息
+        Employee employee = employeedao.get(id);
+        model.addAttribute("emp",employee);
+        //部门查询
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("dept",departments);
         return "emp/listadd";
     }
-    @PostMapping(value = "user/empAdds")
-    public String empAdd(Employee employee){
-        System.out.println("》》");
-        return "redirect:/user/emps";
+    /**
+     * 员工修改---重回员工列表页面
+     */
+    @PutMapping(value = "/empAdds")
+    public String putUpdate(Employee employee){
+        employeedao.save(employee);//后台判断当前的值是否存在，不存在则自增，存在就修改
+        return "redirect:/emps";
     }
 }
